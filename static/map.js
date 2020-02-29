88008ae2d90c1f4c075bcf3709382a981016d668
stionanda <41115559+stionanda@users.noreply.github.com>
2020-02-29 18:04:22 +0000

    Update map.js

--- static/map.js
+++ static/map.js
@@ -3,34 +3,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-map.setView(new L.LatLng(40.737, -73.923), 5);
+map.setView(new L.LatLng(40.7128, -74.0060), 11);
 
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
+var latlng = L.latLng(50.5, 30.5);
 
-L.geoJSON(geojsonFeature).addTo(map);
-
-var geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8
-};
-
-L.geoJSON(someGeojsonFeature, {
-    pointToLayer: function (feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions);
-    }
-}).addTo(map);
+L.circlemarker
