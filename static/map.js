b3d8e7255dd1fef9b1eddd5283044c8d9ad4cbc4
stionanda <41115559+stionanda@users.noreply.github.com>
2020-02-29 20:34:48 +0000

    Add files via upload

--- static/map.js
+++ static/map.js
@@ -1,10 +1,26 @@
 var map = L.map('mapid');
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 map.setView(new L.LatLng(40.7128, -74.0060), 11);
 
-var latlng = L.latLng(50.5, 30.5);
 
-L.circlemarker
+L.marker().addTo(map);
+
+async function asyncCall() {
+	let serialized = await fetch('/static/output.json')
+
+	data = await serialized.json()
+
+	//for (i = 0; i < data.length; i++) {
+  	//	name = String(i)
+  	//	L.marker(data[name]).addTo(map);
+	//}
+	L.marker([50.5, 30.5]).addTo(map);
+}
+
+asyncCall();
+
+
+
