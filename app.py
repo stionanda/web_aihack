2b0fbf8b850eeb13acdbc1ce9d6cdf385b5cde78
stionanda <41115559+stionanda@users.noreply.github.com>
2020-03-01 08:39:29 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 from flask import render_template
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', otto=name)
+#@app.route('/hello/')
+#@app.route('/hello/<name>')
+#def hello(name=None):
+    #return render_template('hello.html', otto=name)
 
 
 @app.route('/index/')
